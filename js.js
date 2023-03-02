197fb86592a4db5dd5dd8c7faae206d61a771949
Hurtik Igor <hurtik.igor@gmail.com>
2023-03-03 00:08:05 +0300

    add js btn to last img block for first img

---
+++ js.js
@@ -0,0 +1,6 @@
+const myButton = document.getElementById('myButton');
+const openButton = document.querySelector('.open-button')
+
+myButton.addEventListener('click', function() {
+    openButton.style.backgroundColor = 'green';
+});
